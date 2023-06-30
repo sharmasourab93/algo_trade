28f7483c36efce07f5d602661eb70a1e1b184a9e
Sourab Sharma <sharmasourab93@users.noreply.github.com>
2023-06-30 23:07:08 +0530

    Write to DF

--- algo_trade/utils/write_df_to_file.py
+++ algo_trade/utils/write_df_to_file.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 def write_df_to_file(data: pd.DataFrame, file_name: str, file_type: str = 'csv'):
-    file_path = os.path.join(path, file_name + '.' + file_type)
+    file_path = os.path.join(ROOT_DIR, 'data', file_name + '.' + file_type)
     
     if file_type == 'csv':
         data.to_csv(file_path)
