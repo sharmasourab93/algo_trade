4c64bcae86c76628b35ab89404b4eae98c90852f
Sourab Sharma <sharmasourab93@users.noreply.github.com>
2023-07-02 22:14:21 +0530

    YFin Updated

--- algo_trade/data_handler/source/yfin/yf_utils.py
+++ algo_trade/data_handler/source/yfin/yf_utils.py
@@ -6,7 +6,7 @@ from datetime import datetime, date
 from calendar import monthrange
 from typing import List, Tuple
 
-from algo_trade.data_handler.calendar.constants import DATE_FMT
+from algo_trade.data_handler.calendar.constants import DATE_FMT, TODAY
 from algo_trade.data_handler.source.constants import YF_UTILS_EXCEPTION_LIST
 from algo_trade.utils.meta import AsyncLoggingMeta
 from algo_trade.data_handler.calendar.calendar_tools import MarketCalendarTools
@@ -24,6 +24,7 @@ class YFUtils(DataUtils, metaclass=AsyncLoggingMeta):
             symbol: str,
             period: str = "1mo",
             interval: str = "1d",
+            date_fmt: str = None,
             rounding: bool = True,
             index: bool = False,
             ascending: bool = False,
@@ -66,161 +67,46 @@ class YFUtils(DataUtils, metaclass=AsyncLoggingMeta):
 
         data.columns = data.columns.str.lower()
 
+        if date_fmt is not None:
+            data["date"] = pd.to_datetime(data.date).apply(lambda x: x.strftime(date_fmt))
+
         return data.round(2)
 
-    def get_unique_ticker_set(self, tickers: list):
+    def get_unique_ticker_set(self, tickers: list) -> tuple:
         tickers = [i + ".NS" for i in tickers]
 
-        return tickers
+        return tuple(tickers)
 
-    def get_previous_1mo_or_1wk_data(
-            self,
-            data: pd.DataFrame,
-            symbol: str,
-            period: str,
-            interval: str) -> pd.DataFrame:
+    def get_previous_quote(self, tickers: list, time_frame: str = "week") -> pd.DataFrame:
 
-        """
-        `get_previous_1mo_or_1wk_data` fetches you
-        the last month's or last week's
-        OHLC data along with % Change  from the previous close
-        of the previous month data from
-        the current month or current week.
-        The purpose of this method is to only
-        serve you the previous week or previous month's data
-        for the selected 'symbol' (stock).
+        _, eom_day = monthrange(self.prev_day.year, self.prev_day.month)
 
-        :param data: Data Frame containing OHLC data
-        :param symbol: Stock Symbol
-        :param period: period in str as required by yfinance
-        :param interval: interval in str as required by yfinance
+        if time_frame == "week":
+            expiry = next(MarketCalendarTools.get_weekly_expiry())
+            expiry = datetime.strptime(expiry, DATE_FMT)
+            date_format = "%d-%b-%Y"
+            if date.today().weekday() not in (0, 1, 2) and datetime.today() <= expiry:
+                period, interval = "3wk", "1wk"
 
-        :returns: Dataframe with OHLC, Prevclose
-                  & % Change for selected Symbol
-        """
-        self.logger.info(
-            "Extracting Bhav for : {0}".format(re.sub(r"\.NS$", "", symbol))
-        )
-        data = data.history(period=period, interval=interval)
-        data.columns = data.columns.str.lower()
-        data["prev_close"] = data.close.shift(1)
-        data = data.reset_index()
-        data["Date"] = pd.to_datetime(data.Date).apply(lambda x: x.date())
-        data.rename(columns={"Date": "date"}, inplace=True)
-        data = data.loc[~data.prev_close.isna(), :]
-
-        if period in ("2mo", "1wk"):
-            # Assuming that when the period is '2mo' or '2wk',
-            # we will only have
-            try:
-                data = pd.DataFrame.from_dict(
-                    [
-                        {
-                            "date": data.date.iloc[0],
-                            "open": data.open.iloc[0],
-                            "high": max(data.high),
-                            "low": min(data.low),
-                            "close": data.close.iloc[-1],
-                            "volume": data.volume.iloc[0] + data.volume.iloc[
-                                1],
-                            "prev_close": data.prev_close.iloc[0],
-                        }
-                    ]
-                )
-            except IndexError:
-                pass
-
-        data = data.head(1).reset_index()
-        data = self.calculate_pct_change(data, "close", "prev_close")
-        data["symbol"] = re.sub(r"\.NS$", "", symbol)
-
-        try:
-            data = data.drop("index", axis=1)
-        except KeyError:
-            pass
-
-        return data
-
-    def get_previous_weekly_quote(self, tickers: list) -> pd.DataFrame:
-        """
-        Get Previous Weekly Quote
-
-        :param tickers: List of Stocks
-        :returns: Previous Weekly Quote.
-
-        """
-        tickers = self.get_unique_ticker_set(tickers)
-        data = yf.Tickers(" ".join(tickers))
-
-        weekly_expiry = datetime.strptime(
-            next(MarketCalendarTools.get_weekly_expiry()), DATE_FMT
-        )
-
-        # If the weekday falls on a Friday, Saturday & Sunday i.e. the day after expiry
-        # and if today is less than or equal to next expiry
-        # Merge the 1wk data with the current day data.
-        # else Only return previous week's data.
-        if date.today().weekday() not in (0, 1, 2) and datetime.today() <= weekly_expiry:
-            data_dict = {
-                i: self.get_previous_1mo_or_1wk_data(data.tickers[i], i,
-                                                     period="1wk", interval="1wk")
-                for i in tickers
-            }
+            else:
+                period, interval = "2wk", "1wk"
 
         else:
-            data_dict = {
-                i: self.get_previous_1mo_or_1wk_data(
-                    data.tickers[i], i, period="2wk", interval="1wk"
-                )
-                for i in tickers
-            }
+            expiry = next(MarketCalendarTools.get_monthly_expiry())
+            expiry = datetime.strptime(expiry, DATE_FMT).date()
+            date_format = "%b-%Y"
+            if TODAY.day <= eom_day and TODAY <= expiry:
+                period, interval = "2mo", "1mo"
+
+            else:
+                period, interval = "3mo", "1mo"
+
+        data_dict = {i: self.get_period_data(i, period, interval, date_format).head(1) for i in tickers}
+
+        for i, j in data_dict.items():
+            j["symbol"] = i
 
         data_dict = pd.concat(data_dict.values())
-        data_dict["date"] = pd.to_datetime(data_dict.date).dt.strftime(
-            "%d-%b-%Y")
-
-        return data_dict
-
-    def get_previous_monthly_quote(self, tickers: list) -> pd.DataFrame:
-        """
-        Get Previous Monthly Quote
-
-        :param tickers: List of Stocks
-        :returns: Previous Monthly Quote.
-        """
-        # TODO: Needs fixing.
-        tickers = self.get_unique_ticker_set(tickers)
-        data = yf.Tickers(" ".join(tickers))
-
-        monthly_expiry = datetime.strptime(
-            next(MarketCalendarTools.get_monthly_expiry()), DATE_FMT
-        )
-        _, eom_day = monthrange(date.today().year, date.today().month)
-
-        # If the day is less than the day in the calendar month
-        # and today is less than the monthly expiry which is the current month's expiry
-        # Consume the second data from the 3mo period on 1mo interval
-        # else return curent month's data including the last day until month completes.
-
-        if date.today().day < eom_day and datetime.today() < monthly_expiry:
-
-            data_dict = {
-                i: self.get_previous_1mo_or_1wk_data(
-                    data.tickers[i], i, period="3mo", interval="1mo"
-                )
-                for i in tickers
-            }
-
-        else:
-            data_dict = {
-                i: self.get_previous_1mo_or_1wk_data(
-                    data.tickers[i], i, period="2mo", interval="1mo"
-                )
-                for i in tickers
-            }
-
-        data_dict = pd.concat(data_dict.values())
-        data_dict["date"] = pd.to_datetime(data_dict.date).dt.strftime("%b-%Y")
 
         return data_dict
 
@@ -240,12 +126,7 @@ class YFUtils(DataUtils, metaclass=AsyncLoggingMeta):
         :returns: Data Frame with All Symbols with previous OHLC data & % Change
         """
 
-        time_methods = {
-            "week": self.get_previous_weekly_quote,
-            "month": self.get_previous_monthly_quote,
-        }
-
-        return time_methods[timeframe](tickers)
+        return self.get_previous_quote(tickers, timeframe)
 
     def pullback_quote_generator(self,
                                  ticker: str,
@@ -269,8 +150,9 @@ class YFUtils(DataUtils, metaclass=AsyncLoggingMeta):
 
         return data
 
-    def larger_timeframe_quotes(self, tickers: str, days: tuple = (30, 90,
-                                                                   180),
+    def larger_timeframe_quotes(self,
+                                tickers: str,
+                                days: tuple = (30, 90, 180),
                                 period: str = '6mo',
                                 interval: str = '1d'):
 
