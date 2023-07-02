a17be88e8a64426551d1633659a5b70d01c7ec20
Sourab Sharma <sharmasourab93@users.noreply.github.com>
2023-07-02 22:47:11 +0530

    Swing Run Update

--- algo_trade/market/strategy/scanners/swing/swing_run.py
+++ algo_trade/market/strategy/scanners/swing/swing_run.py
@@ -3,6 +3,7 @@ from inspect import isclass, getmembers
 from algo_trade.utils import write_df_to_file
 from algo_trade.data_handler.calendar.constants import DATE_FMT
 
+from algo_trade.market.strategy.scanners.scanner_generics.swing_generic import store_swing_output, retrieve_swing_output
 from algo_trade.market.strategy.scanners.swing.swing_buying_pullback import \
     SwingBuyingPullBackScanner
 from algo_trade.market.strategy.scanners.swing.swing_volume_scanner import \
@@ -13,21 +14,27 @@ from algo_trade.market.strategy.scanners.swing.swing_ema_crossover_scanner \
 
 def swing_result():
     class_list = list()
+    data = retrieve_swing_output()
+    if data is not None:
+        return data
+
     for name, obj in getmembers(sys.modules[__name__]):
         if isclass(obj):
             _ = obj()
             class_list.append(_)
-    
+
     class_list = [i.generate_swing_output() for i in class_list]
-    
-    return _.get_swing_result()
+
+    data = _.get_swing_result()
+    store_swing_output(data)
+    return data
 
 
 if __name__ == '__main__':
     result = swing_result()
-    
+
     from datetime import datetime
-    
+
     dated_ = datetime.today().strftime(DATE_FMT)
-    
+
     write_df_to_file(result, "{0}_{1}".format("SWINGRUN", dated_))
