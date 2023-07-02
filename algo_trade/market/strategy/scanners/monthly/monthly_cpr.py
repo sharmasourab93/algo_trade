3a6e0766e023c65acb1ebd4cd43cc094b4355a2a
Sourab Sharma <sharmasourab93@users.noreply.github.com>
2023-07-02 21:38:15 +0530

    Monthly & Weekly CPR Corrections

--- algo_trade/market/strategy/scanners/monthly/monthly_cpr.py
+++ algo_trade/market/strategy/scanners/monthly/monthly_cpr.py
@@ -13,22 +13,23 @@ class MonthlyCprStrategy(CPRAbstract, metaclass=AsyncLoggingMeta):
 
     def cpr_strategy_output(self) -> pd.DataFrame:
 
-        self.logger.debug("Initialized Monthly CPR Process")
+        self.logger.info("Initialized Monthly CPR Process")
 
         market_expiry = datetime.strptime(next(
             MarketCalendarTools.get_monthly_expiry()), DATE_FMT).date()
 
-        # On the day when the prev day falls on the market expiry
-        # and the next day is greater than the market expiry, we execute
-        # the monthly cpr strategy process.
+        # On the Previous day month and current day month not being the same
+        # Monthly CPR is executed.
 
-        if self.yf_utils.prev_day <= market_expiry < self.yf_utils.next_day:
+        if self.yf_utils.prev_day.month != self.yf_utils.next_day.month:
 
             data = self.yf_utils.processed_timeframe_bhavcopy(self.nse_n,
                                                               "month")
 
+            self.logger.info("Monthly Quote DataFrame receivied. "
+                             "Shape: {0}".format(data.shape))
             data = self.plot_pivots_with_cpr(data)
-
+            self.logger.info("Monthly CPR plotted.")
             cpr_columns = CPR_STRATEGY_COLUMNS
 
             if self.order_by is not None:
@@ -39,18 +40,18 @@ class MonthlyCprStrategy(CPRAbstract, metaclass=AsyncLoggingMeta):
                                                      ascending=self.asc)
 
             except KeyError:
-                cpr_columns.remove("con_range")
+                cpr_columns.remove("time_con_range")
                 cpr_columns.remove("purpose")
                 data = data[cpr_columns].sort_values(by=self.order_by,
                                                      ascending=self.asc)
 
             data["frequency"] = self.CPR_FREQUENCY
 
-            self.logger.debug(
+            self.logger.info(
                 "Execution of Weekly CPR Strategy Scanner Complete. "
                 "Data Shape: {0}.".format(data.shape)
             )
-
+            data = data.loc[~(data.priority_stocks == '-'), :].sort_values(by=['cpr_width'])
             return data.round(2)
 
         return pd.DataFrame()
